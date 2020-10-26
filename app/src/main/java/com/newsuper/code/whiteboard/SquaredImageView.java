33d1c9bc123f87332c9181f2e0ed02155c9b8ac1
NewChao <newsuper6892@sina.com>
2020-10-26 14:40:30 +0800

    一个可涂鸦、绘图、添加文字、图像（可旋转缩放）、背景的Fragment，其中主要由SketchView利用matrix完成所有图形绘制操作。

---
+++ app/src/main/java/com/newsuper/code/whiteboard/SquaredImageView.java
@@ -0,0 +1,21 @@
+package com.newsuper.code.whiteboard;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+class SquaredImageView extends ImageView {
+    public SquaredImageView(Context context) {
+        super(context);
+    }
+
+    public SquaredImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
+    }
+}
