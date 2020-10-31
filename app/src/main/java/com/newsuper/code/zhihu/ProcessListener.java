ac920dbf8e28c4b6fed734f044f3dd8c3ab150ee
NewChao <newsuper6892@sina.com>
2020-10-31 18:15:56 +0800

    仿知乎view

---
+++ app/src/main/java/com/newsuper/code/zhihu/ProcessListener.java
@@ -0,0 +1,5 @@
+package com.newsuper.code.zhihu;
+
+public interface ProcessListener {
+    void onProcessFinished(int width, int height);
+}
---
+++ app/src/main/java/com/newsuper/code/zhihu/ReScalePostprocessor.java
@@ -0,0 +1,47 @@
+package com.newsuper.code.zhihu;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.util.Log;
+
+import com.facebook.common.references.CloseableReference;
+import com.facebook.imagepipeline.bitmaps.PlatformBitmapFactory;
+import com.facebook.imagepipeline.request.BasePostprocessor;
+
+
+public class ReScalePostprocessor extends BasePostprocessor {
+
+    private int width;
+    private ProcessListener listener;
+
+    private int scaledWidth;
+    private int scaledHeight;
+
+    public ReScalePostprocessor(int width, ProcessListener listener) {
+        this.width = width;
+        this.listener = listener;
+    }
+
+    @Override
+    public CloseableReference<Bitmap> process(Bitmap sourceBitmap, PlatformBitmapFactory bitmapFactory) {
+        float scale = 1.0f * width / sourceBitmap.getWidth();
+        Log.e("ReScalePostprocessor", "scale:" + scale);
+
+        scaledWidth = (int) (sourceBitmap.getWidth() * scale);
+        scaledHeight = (int) (sourceBitmap.getHeight() * scale);
+
+        listener.onProcessFinished(scaledWidth, scaledHeight);
+
+        Matrix matrix = new Matrix();
+        matrix.postScale(scale, scale);
+        Bitmap bitmap = Bitmap.createBitmap(sourceBitmap, 0, 0, sourceBitmap.getWidth(), sourceBitmap.getHeight(), matrix, true);
+        CloseableReference<Bitmap> bitmapRef = bitmapFactory.createBitmap(bitmap);
+        try {
+            return CloseableReference.cloneOrNull(bitmapRef);
+        } finally {
+            CloseableReference.closeSafely(bitmapRef);
+        }
+    }
+}
